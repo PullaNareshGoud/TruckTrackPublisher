fce69dbd2f9e4899434ac685436458a45100498b
PULLA NARESH GOUD <pullanareshgoud@gmail.com>
2021-06-03 23:05:47 +0530

    Json to xml conversion API implemented lombok added slf4j added

--- src/main/java/com/hlc/TruckTrackPublisher/controller/PublishController.java
+++ src/main/java/com/hlc/TruckTrackPublisher/controller/PublishController.java
@@ -3,15 +3,19 @@ package com.hlc.TruckTrackPublisher.controller;
 import ionic.Msmq.Message;
 import ionic.Msmq.MessageQueueException;
 import ionic.Msmq.Queue;
+import lombok.extern.slf4j.Slf4j;
+import org.json.JSONObject;
+import org.json.XML;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.UnsupportedEncodingException;
 import java.util.UUID;
 
 @RestController
+@Slf4j
 
 public class PublishController {
 
@@ -19,11 +23,31 @@ public class PublishController {
     Queue queue;
     @PostMapping("/publish")
     public void publish(@RequestBody String payload) throws MessageQueueException, UnsupportedEncodingException {
-        System.out.println("Sending.....");
+        log.info("Sending.....");
 
         Message msg= new Message(payload, "inserted by TruckTrackPublisher", UUID.randomUUID().toString());
 
         queue.send(msg);
-        System.out.println("Message sent to queue!!");
+        log.info("Message sent to queue!!");
+    }
+
+    @PostMapping(value = "/publish-json", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void publishJson(@RequestBody String payload) throws MessageQueueException, UnsupportedEncodingException {
+
+        log.info("Converting json to xml.....");
+        JSONObject json = new JSONObject(payload);
+        String xml = XML.toString(json);
+        log.info(" xml data after conversion ....."+ xml);
+        log.info("Sending xml .....");
+
+        Message msg= new Message(xml, "inserted by TruckTrackPublisher", UUID.randomUUID().toString());
+
+        queue.send(msg);
+        log.info("Message sent to queue!!");
+    }
+
+    @ExceptionHandler
+    public ResponseEntity exception(Exception e){
+        return ResponseEntity.badRequest().body(e.getMessage());
     }
 }
